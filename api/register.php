a052b532317953c51aaa83e345b506316e6c30b9
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-11-02 22:32:13 +0200

    pe acolo

--- api/register.php
+++ api/register.php
@@ -5,19 +5,20 @@ if (isset($_POST['submit'])) {
 
     $user = new User();
 
-    if(Input::get("password1") == Input::get("password2")){
-    try {
-        $user->create(array(
-            'username' => Input::get('username'),
-            'email' => Input::get('email'),
-            'password' => hash("sha256", Input::get("password1") . Input::get("salt")),
-            'salt' => Input::get("salt")
-        ));
+    if (Input::get("password1") == Input::get("password2")) {
+        try {
+            $user->create(array(
+                'username' => Input::get('username'),
+                'email' => Input::get('email'),
+                'password' => hash("sha256", Input::get("password1") . Input::get("salt")),
+                'salt' => Input::get("salt")
+            ));
 
-        Redirect::to("../login.php");
-    } catch (Exception $e) {
-        $e->getMessage();
-    }}else{
-        Redirect::to("../login.php?parole=false")
+            Redirect::to("../login.php");
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    } else {
+        Redirect::to("../login.php?parole=false");
     }
 }

979ef448b0b5ef34fac05eb45525e3b44fc4fd7b
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-11-10 01:20:23 +0200

    mor

--- api/post.php
+++ api/post.php
@@ -1,40 +1,40 @@
 <?php
-// require_once '../core/init.php';
-require_once '../initCloudinary.php';
+require_once '../core/init.php';
+// require_once '../initCloudinary.php';
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
-$active_group = 'default';
-$query_builder = TRUE;
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"], 1);
+// $active_group = 'default';
+// $query_builder = TRUE;
 
 
-$GLOBALS['config'] = array(
-    'mysql' => array(
-        'host' => $cleardb_server,
-        "username" => $cleardb_username,
-        'password' => $cleardb_password,
-        'db' => 'heroku_81e42f634c069bd'
-    ),
-    'session' => array(
-        'session_name' => 'user',
-        'token_name' => 'token'
-    )
-);
+// $GLOBALS['config'] = array(
+//     'mysql' => array(
+//         'host' => $cleardb_server,
+//         "username" => $cleardb_username,
+//         'password' => $cleardb_password,
+//         'db' => 'heroku_81e42f634c069bd'
+//     ),
+//     'session' => array(
+//         'session_name' => 'user',
+//         'token_name' => 'token'
+//     )
+// );
 
-// require_once '../core/init.php';
-require_once '../classes/Config.php';
+// // require_once '../core/init.php';
+// require_once '../classes/Config.php';
 
-require_once '../classes/DB.php';
+// require_once '../classes/DB.php';
 
-require_once '../classes/Input.php';
-require_once '../classes/Redirect.php';
-require_once '../classes/User.php';
-require_once '../classes/Session.php';
+// require_once '../classes/Input.php';
+// require_once '../classes/Redirect.php';
+// require_once '../classes/User.php';
+// require_once '../classes/Session.php';
 
-require_once '../classes/Admin.php';
+// require_once '../classes/Admin.php';
 
 require_once '../initCloudinary.php';
 use Cloudinary\Api\Upload\UploadApi;

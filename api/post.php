a43c387dd47edd7bd71c6644a3ece63e80edee57
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-11-09 23:18:01 +0200

    mor

--- api/post.php
+++ api/post.php
@@ -1,26 +1,45 @@
 <?php
 require_once '../core/init.php';
+require_once '../initCloudinary.php';
+
+use Cloudinary\Api\Upload\UploadApi;
+
+
 
 if (isset($_POST['submit'])) {
 
     $admin = new Admin();
 
-    Input::moveImg(array("img"));
+
+    // \Cloudinary\Uploader::upload(
+    //     $_FILES['img']['tmp_name'],
+    //     array(
+    //         "public_id" => 'a'
+    //     )
+    // );
+
+    $up = new UploadApi();
+    $up->upload($_FILES['img']['tmp_name'], array(
+        "public_id"=>"alina"
+    ));
+
+
+    print_r($up);
 
     try {
         $rez =   $admin->addPost(array(
-            "poza" => "./assets/img/".Input::getFILE("img")['name'],
+            "poza" => "./assets/img/" . Input::getFILE("img")['name'],
             "user" => Input::get("user"),
             "sezon" => Input::get("sezon"),
             "descriere" => Input::get("descriere"),
             "users" => "[]",
-            "gen"=>Input::get("gen")
+            "gen" => Input::get("gen")
         ));
         $path = "../admin.php?success=";
 
         $rez ? $path .= "true" : $path .= "false";
 
-        Redirect::to($path);
+        // Redirect::to($path);
     } catch (Exception $e) {
         die($e->getMessage());
     }

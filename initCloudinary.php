33c477e9f2c3f31e052a68ab92db9dba7799fa58
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-11-10 00:53:24 +0200

    mor

--- initCloudinary.php
+++ initCloudinary.php
@@ -13,12 +13,14 @@ Configuration::instance([
       use Cloudinary\Api\Upload\UploadApi;
 $r = new UploadApi();
 $r = $r->upload($_FILES['img']['tmp_name']);
-
+echo '<pre>';
 print_r($r);
+echo '</pre>';
 // /Cloudinary::config([ 
 //     "cloud_name" => "thobor", 
 //     "api_key" => "745867666258536", 
 //     "api_secret" => "Hnn5N_wn08EQj8YUt59BYRTSvKc", 
 //     "secure" => true]);
+
 
 ?>

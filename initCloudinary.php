7a587797f788e282704c36f798cd232bcb2a68b3
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-11-10 00:43:18 +0200

    mor

--- initCloudinary.php
+++ initCloudinary.php
@@ -1,8 +1,22 @@
 <?php
-// require_once './vendor/autoload.php';
+require_once 'vendor/autoload.php';
+use Cloudinary\Configuration\Configuration;
 
-\Cloudinary::config([ 
-    "cloud_name" => "thobor", 
-    "api_key" => "745867666258536", 
-    "api_secret" => "Hnn5N_wn08EQj8YUt59BYRTSvKc", 
-    "secure" => true]);
+Configuration::instance([
+    'cloud' => [
+      'cloud_name' => 'thobor', 
+      'api_key' => '745867666258536', 
+      'api_secret' => 'Hnn5N_wn08EQj8YUt59BYRTSvKc'],
+    'url' => [
+      'secure' => true]]);
+
+      use Cloudinary\Api\Upload\UploadApi;
+(new UploadApi())->upload('work3.jpeg')
+
+// /Cloudinary::config([ 
+//     "cloud_name" => "thobor", 
+//     "api_key" => "745867666258536", 
+//     "api_secret" => "Hnn5N_wn08EQj8YUt59BYRTSvKc", 
+//     "secure" => true]);
+
+?>
